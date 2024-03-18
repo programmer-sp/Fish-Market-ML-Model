c166f926a5dec70a8a81a52f44ef2d3cc95452e2
Sagar Parmar <parmarsagar.js@gmail.com>
2024-03-18 08:19:10 -0400

    chore: minor changes

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 from flask import Flask, render_template, request
-import pickle
-import numpy as np
+# import pickle
+# import numpy as np
 
 app = Flask(__name__)
 
 # Load the trained model
-with open('fish_market_model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+# with open('fish_market_model.pkl', 'rb') as model_file:
+#     model = pickle.load(model_file)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    # Get user input
-    length1 = float(request.form['length1'])
-    length2 = float(request.form['length2'])
-    length3 = float(request.form['length3'])
-    height = float(request.form['height'])
-    width = float(request.form['width'])
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     # Get user input
+#     length1 = float(request.form['length1'])
+#     length2 = float(request.form['length2'])
+#     length3 = float(request.form['length3'])
+#     height = float(request.form['height'])
+#     width = float(request.form['width'])
 
-    # Make prediction
-    features = np.array([[length1, length2, length3, height, width]])
-    prediction = model.predict(features)
+#     # Make prediction
+#     features = np.array([[length1, length2, length3, height, width]])
+#     prediction = model.predict(features)
 
-    return render_template('index.html', prediction=prediction[0])
+#     return render_template('index.html', prediction=prediction[0])
 
 if __name__ == '__main__':
     app.run(debug=True)
